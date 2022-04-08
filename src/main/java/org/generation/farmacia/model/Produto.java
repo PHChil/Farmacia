cc50eb9bdf16d5d8afad05878fcdb61dc700a3b0
PHChil <paulo_henriquechil@hotmail.com>
2022-04-08 18:30:16 -0300

    Farmacia

---
+++ src/main/java/org/generation/farmacia/model/Produto.java
@@ -0,0 +1,71 @@
+package org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name="db_produto")
+
+
+public class Produto {
+	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	@NotNull
+	private String nome;
+	@NotNull
+	private String descricao;
+	@NotNull
+	private double preco;
+	@NotNull
+	private int estoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+	
+	
+	
+	}
+	
+	
